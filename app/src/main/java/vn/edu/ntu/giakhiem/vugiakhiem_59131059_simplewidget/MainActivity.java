76798f874294eebfbf2a3a9eed92949365c512c0
Khiem <vgkhiem.9.8.c2pctrinh@gmail.com>
2020-04-23 19:02:29 +0700

    v1

---
+++ app/src/main/java/vn/edu/ntu/giakhiem/vugiakhiem_59131059_simplewidget/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.ntu.giakhiem.vugiakhiem_59131059_simplewidget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
