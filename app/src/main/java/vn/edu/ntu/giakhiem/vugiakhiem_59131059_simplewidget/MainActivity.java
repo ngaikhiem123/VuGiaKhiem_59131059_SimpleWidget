bf49e7392d84ae68315ff1b40b68a4abaa29d830
Khiem <vgkhiem.9.8.c2pctrinh@gmail.com>
2020-04-23 22:55:34 +0700

    v3

--- app/src/main/java/vn/edu/ntu/giakhiem/vugiakhiem_59131059_simplewidget/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/giakhiem/vugiakhiem_59131059_simplewidget/MainActivity.java
@@ -3,7 +3,9 @@ package vn.edu.ntu.giakhiem.vugiakhiem_59131059_simplewidget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -16,9 +18,8 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText edtName,edtDay;
+    EditText edtName, edtDay, edtSoThich;
     RadioGroup rdgGioiTinh;
-
     Button btnXN;
 
     @Override
@@ -29,35 +30,65 @@ public class MainActivity extends AppCompatActivity {
         addEvent();
     }
 
-    private void addViews(){
+    private void addViews() {
         edtName = findViewById(R.id.edtName);
         edtDay = findViewById(R.id.edtDay);
         rdgGioiTinh = findViewById(R.id.rdgGioiTinh);
+        edtSoThich = findViewById(R.id.edtSoThich);
         btnXN = findViewById(R.id.btnXN);
     }
-    private void addEvent(){
+
+    private void addEvent() {
         btnXN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String sName = edtName.getText().toString();
-                if (sName.matches("")) sName = "Chưa rõ họ tên";
+                String sName = getName();
+                String sGioiTinh = getGioiTinh();
+                String sSoThich = getSoThich();
 
-                int selectedBtn = rdgGioiTinh.getCheckedRadioButtonId();
-                String sGioiTinh = "Giới tính: " + ((Button)findViewById(selectedBtn)).getText().toString();
-
-                //String sCheckbox = getCheckedBox((LinearLayout)findViewById(R.id.mainLayout));
-                String sKQ = sName + "\n" + sGioiTinh;
-                Toast.makeText(getApplicationContext(),sKQ,Toast.LENGTH_SHORT).show();
+                String sKQ = sName + "\n" + sGioiTinh + "\nSở Thích: " + sSoThich;
+                Toast.makeText(getApplicationContext(), sKQ, Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    private String getCheckedBox(LinearLayout linearLayout){
+    private String getName (){
+        String s = edtName.getText().toString();
+        if (s.matches("")) s = "Chưa rõ họ tên";
+        return s;
+    }
+
+    private String getGioiTinh (){
+        int selectedBtn = rdgGioiTinh.getCheckedRadioButtonId();
+        return "Giới tính: " + ((Button) findViewById(selectedBtn)).getText().toString();
+    }
+
+    private String getSoThich() {
+        String s, s1 = getCheckedBox(), s2 = edtSoThich.getText().toString();
+
+        if (!s1.matches("") && !s2.matches(""))
+            return s1 + "; " + s2;
+        else if (s1.matches("") && s2.matches(""))
+            return "chưa rõ";
+        else return s1 + s2;
+    }
+
+    private String getCheckedBox() {
+        ViewGroup viewgroup = (ViewGroup) findViewById(R.id.mainLayout);
         String s = "";
-        for (int i = 0 ; i < linearLayout.getChildCount(); i++){
-            View v= linearLayout.getChildAt(i);
-            if (((CheckBox)v).isChecked()) s +=((CheckBox) v).getText().toString();
+        boolean kt = false;
+        for (int i = 0; i < viewgroup.getChildCount(); i++) {
+            View v1 = viewgroup.getChildAt(i);
+            if (v1 instanceof CheckBox)
+                if (((CheckBox) v1).isChecked())
+                    if (kt)
+                        s += "; " + ((CheckBox) v1).getText().toString();
+                    else {
+                        s += ((CheckBox) v1).getText().toString();
+                        kt = true;
+                    }
         }
         return s;
     }
+
 }
